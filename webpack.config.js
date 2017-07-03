c549a5c9f0b7937f0a81c3fa5d4dde7a9c2c8461
youxingzhi <youxingzhi@qq.com>
2017-07-03 10:25:52 +0800

    add .npmignore file, update package 'main'

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'vue-date-range.js',
+    filename: 'vue-date-range.min.js',
     library: 'daterange',
     libraryTarget: 'umd'
   },
